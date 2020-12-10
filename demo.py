267cfb6a77153bf6ade6826672eab6a934ff9a38
意志减肥监督局 <gxj@yizhijianfeijiandujudeMacBook-Pro.local>
2020-12-10 22:49:05 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+print(2+2)
+print('c:\\some\\name')
+print(r'c:\some\name')
+print('Py' 'thon')
+x='abc'
+print(f'c:\\some\\name\\{x}')
+print('c:\\some\\name\\{}'.format(x))
+print('c:\\some\\name\\{}\\{}'.format(x,123))
+print(r'c:\some\name\{}\{}'.format(x,123))
